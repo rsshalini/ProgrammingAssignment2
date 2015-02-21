85b2c99312a2fac8da20e5845b082eedec247ad4
Shalini Subramanian <shalini.916@gmail.com>
2015-02-20 22:51:39 -0800

    Wrote details of Caching the inverse of a matrix
    Wrote details of Caching the inverse of a matrix. The first part of the
    function is makeCacheMatrix where a matrix input is defined by user.
    The second part is the cacheSolve where the inverse of the matrix is
    calculated or retrieved from the calculated data (cache).

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix takes matrix as an argument.If no argument is given, then makeCacheMatrix will create a empty numeric matrix
+makeCacheMatrix <- function(x = numeric()) {  
+	i <- NULL 
+## Function starts with setting inverse to NULL as a placeholder for a future value
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+## defines a function to set the matrix x to a matrix y and resets the inverse, i to NULL.
+	get <- function() x 
+## returns the matrix, x.
+	setinverse <- function(inverse)  i <<- inverse
+## sets the inverse, i, to inverse
+	getinverse <- function() i
+## returns the inverse, i
+	list (set = set, get= get,
+		  setinverse = setinverse,
+		  getinverse = getinverse)
 }
+## returns the 'special matrix' containing all of the functions just defined. 
 
 
-## Write a short comment describing this function
+## this function calculates the inverse of the matrix and returns. If the inverse is already caculated, the inverse of x is stored in variable i. 
+## It retrieves the inverse from the variable i and prints the messgage that it is getting cached data and then the value of i.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		i <- x$getinverse()
+		if(!is.null(i){
+	       message("getting cached data")
+	       return(i)
+		   } 
+## if the inverse is calculated already, variable i will not be emplty. Hence, the message is printed and then the value of the inverse of the matrix is retrieved.
+		   data <- x$get()
+		   i <- solve(data, ...)  
+## inverse is calculated in this function and returned for the first time. 
+	   x$setinverse(i) 
+	   i
 }
